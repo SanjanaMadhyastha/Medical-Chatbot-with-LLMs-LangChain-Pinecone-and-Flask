1a840cbc590962c97998b691311b5e816808dd18
Sanjana <sanjanamadhyastha21@gmail.com>
2025-09-25 08:29:51 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Sanjana Madhyastha",
+    author_email="sanjanamadhyastha@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
